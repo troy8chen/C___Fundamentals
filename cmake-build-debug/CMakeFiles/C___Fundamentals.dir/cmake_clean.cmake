ba1536909aa48e7df90e7d512e6d0d570d5186a5
troy8chen <troy8chen@gmail.com>
2022-02-03 15:45:27 -0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/C___Fundamentals.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C___Fundamentals.dir/main.cpp.o"
+  "C___Fundamentals"
+  "C___Fundamentals.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C___Fundamentals.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
